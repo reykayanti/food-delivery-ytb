bd67ca7c7287472f720eae796fc7db254e134676
Retno Ekayanti <retnoekayanti15@gmail.com>
2025-02-04 14:20:49 +0700

    shop Cart in Frontend

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -10,17 +10,23 @@ const StoreContextProvider = (props) => {
     const [token, setToken] = useState("")
     const [food_list, setfoodList] = useState([])
 
-    const addToCart = (itemId) =>  {
+    const addToCart = async (itemId) =>  {
         if(!cartItems[itemId]) {
             setCartItems((prev)=>({...prev, [itemId]:1}))
         }
         else {
             setCartItems((prev)=>({...prev, [itemId]:prev[itemId] + 1}))
         }
+        if (token) {
+            await axios.post(url+ "/api/cart/add", {itemId}, {headers:{token}})
+        }
     }
 
-    const removeFromCart = (itemId) =>  {
+    const removeFromCart = async (itemId) =>  {
         setCartItems((prev)=>({...prev, [itemId]:prev[itemId]-1}))
+        if (token) {
+            await axios.post(url+ "/api/cart/remove", {itemId}, {headers:{token}})
+        }
     }
 
     const getTotalCartAmount = () => {
@@ -44,12 +50,18 @@ const StoreContextProvider = (props) => {
         setfoodList(response.data.data)
     }
 
+    const loadCartData = async (token) => {
+        const response = await axios.post(url + "/api/cart/get", {}, {headers: {token}})
+        setCartItems(response.data.cartData)
+    }
+
 
     useEffect(()=>{
         async function loadData() { // Mendeklarasikan fungsi async untuk memuat data
             await fetchFoodList(); // Menunggu hasil dari pemanggilan fetchFoodLis
             if (localStorage.getItem("token")) {  // Memeriksa apakah ada "token" yang tersimpan di localStorage
                 setToken(localStorage.getItem("token"))  // Jika ada, set nilai token ke state setToken
+                await loadCartData(localStorage.getItem("token"))
             }
         }
         loadData(); //memanggil fungsi loadData
