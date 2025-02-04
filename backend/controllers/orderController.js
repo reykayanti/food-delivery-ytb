ce456f4b52e7fffc4d8a870e84191f461b8582cf
Retno Ekayanti <retnoekayanti15@gmail.com>
2025-02-04 15:59:06 +0700

    order with stripe

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,27 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+// placing user order for frontend
+const placeOrder = async (req, res) => {
+
+    try {
+        const newOrder = new orderModel({
+            userId: req.body.userId,
+            items: req.body.items,
+            amount: req.body.amount,
+            address: req.body.address,
+        })
+        await newOrder.save();
+        await userModel.findByIdAndUpdate(req.body.userId, {cartData : {}})
+
+        const line_items = req.body.items.map()
+
+    } catch (error) {
+        
+    }
+}
+
+export {placeOrder};
