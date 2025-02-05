84ebbc68acd5bb6beca75f53933bf6cde16c50dd
Retno Ekayanti <retnoekayanti15@gmail.com>
2025-02-05 10:19:16 +0700

    order in backend and frontend using stripe

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,20 +7,61 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 // placing user order for frontend
 const placeOrder = async (req, res) => {
 
+    const frontend_url = "http://localhost:5173"
+
     try {
+        //Membuat instance baru dari model order dengan data yang diterima dari request body
         const newOrder = new orderModel({
             userId: req.body.userId,
             items: req.body.items,
             amount: req.body.amount,
             address: req.body.address,
         })
-        await newOrder.save();
-        await userModel.findByIdAndUpdate(req.body.userId, {cartData : {}})
 
-        const line_items = req.body.items.map()
+        // Menyimpan order baru ke database
+        await newOrder.save(); 
+
+        // Menghapus keranjang belanja pengguna setelah memesan
+        await userModel.findByIdAndUpdate(req.body.userId, {cartData : {}}) 
+
+        // Membuat array line_items untuk Stripe Checkout berdasarkan item yang dipesan
+        const line_items = req.body.items.map((item)=>({
+            price_data:{
+                currency: "idr", // Mata uang yang digunakan
+                product_data:{
+                    name:item.name // Nama produk
+                },
+                unit_amount:item.price*100*16000 // Menghitung harga dalam satuan terkecil
+            },
+            quantity:item.quantity // Jumlah item yang dipesan
+        }))
+
+        // Menambahkan biaya pengiriman ke dalam line_items
+        line_items.push({
+            price_data: {
+                currency: "idr", // Mata uang yang digunakan
+                product_data: {
+                    name: "Delivery Charges" // Nama untuk biaya pengiriman
+                },
+                unit_amount: 2 * 100 * 16000 // Menghitung biaya pengiriman dalam satuan terkecil
+            },
+            quantity: 1 // Jumlah biaya pengiriman (biasanya 1)
+        });
+
+        // Membuat sesi checkout baru di Stripe dengan line_items yang telah disiapkan
+        const session = await stripe.checkout.sessions.create({
+            line_items: line_items, // Item yang akan ditampilkan di checkout
+            mode: 'payment', // Mode pembayaran
+            success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`, // URL untuk sukses
+            cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}` // URL untuk pembatalan
+        });
+
+        // Mengirimkan respons JSON dengan status sukses dan URL sesi checkout
+        res.json({success:true, session_url:session.url})
 
     } catch (error) {
-        
+        console.log(error);
+        res.json({success:false, message:"Error"});
     }
 }
 
--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,32 +1,91 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './PlaceOrder.css'
 import { StoreContext } from '../../context/StoreContext';
+import axios from 'axios';
 
 export const PlaceOrder = () => {
 
-  const { getTotalCartAmount } = useContext(StoreContext);
+  //Mengambil nilai dari StoreContext
+  const { getTotalCartAmount, token, food_list, cartItems, url } = useContext(StoreContext);
+
+  // Menginisialisasi state untuk menyimpan data form pengguna
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    street: "",
+    city: "",
+    state : "",
+    zipcode : "",
+    country : "",
+    phone : "",
+  })
+
+  // Fungsi untuk menangani perubahan pada input form
+  const onChangeHandler = (event) => {
+    const name = event.target.name; // Mengambil nama input yang berubah
+    const value = event.target.value; // Mengambil nilai input yang baru
+    setData(data=>({...data, [name]:value})) // Memperbarui state dengan nilai baru, menjaga nilai yang sudah ada
+  }
+
+  // cek perubahan data pada console. hanya untuk cek saja
+  // useEffect(()=>{
+  //   console.log(data);
+  // }, [data])
+
+  const placeOrder = async (event) => {
+    event.preventDefault(); // mencegah browser melakukan reload halaman ketika form disubmit
+    let orderItems = []; //Inisialisasi array kosong untuk menyimpan item pesanan\
+
+    //Mengiterasi setiap item dalam food_list
+    food_list.map((item) => {
+      if (cartItems[item._id]>0) { // jika item lebih dari 0
+        let itemInfo = item;
+        itemInfo["quantity"] = cartItems[item._id] // Menambahkan properti 'quantity' ke itemInfo dengan jumlah dari cartItems
+        orderItems.push(itemInfo) // Menambahkan itemInfo yang telah dimodifikasi ke dalam array orderItems
+      }
+    })
+
+    //Membuat objek orderData yang berisi informasi pesanan
+    let orderData = {
+      address:data,
+      items:orderItems,
+      amount:getTotalCartAmount()+2,
+    }
+
+    //// Mengirimkan permintaan POST ke server untuk membuat pesanan baru
+    let response = await axios.post(url+"/api/order/place", orderData, {
+      headers:{token} /// Menyertakan token dalam header untuk otentikasi
+    })
+    if (response.data.success){
+      const {session_url} = response.data;
+      window.location.replace(session_url);
+    } else {
+      alert("Error")
+    }
+  }
 
   return (
-    <form className='place-order'>
+    <form onSubmit={placeOrder} className='place-order'>
       <div className="place-order-left">
         <p className="title">
           Delivery Information
         </p>
         <div className="multi-fields">
-          <input type="text" placeholder='First Name' />
-          <input type="text" placeholder='Last Name' />
+          <input required name='firstName' onChange={onChangeHandler} value={data.firstName} type="text" placeholder='First Name' />
+          <input required name='lastName' onChange={onChangeHandler} value={data.lastName} type="text" placeholder='Last Name' />
         </div>
-        <input type="email" placeholder='Email Adress' />
-        <input type="text" placeholder='Street' />
+        <input required name='email' onChange={onChangeHandler} value={data.email} type="email" placeholder='Email Adress' />
+        <input required name='street' onChange={onChangeHandler} value={data.street} type="text" placeholder='Street' />
         <div className="multi-fields">
-          <input type="text" placeholder='City' />
-          <input type="text" placeholder='State' />
+          <input required name='city' onChange={onChangeHandler} value={data.city} type="text" placeholder='City' />
+          <input required name='state' onChange={onChangeHandler} value={data.state} type="text" placeholder='State' />
         </div>
         <div className="multi-fields">
-          <input type="text" placeholder='Zip Code' />
-          <input type="text" placeholder='Country' />
+          <input required name='zipcode' onChange={onChangeHandler} value={data.zipcode} type="text" placeholder='Zip Code' />
+          <input required name='country' onChange={onChangeHandler} value={data.country} type="text" placeholder='Country' />
         </div>
-        <input type="text" placeholder="Phone" />
+        <input required name='phone' onChange={onChangeHandler} value={data.phone} type="text" placeholder="Phone" />
       </div>
 
       <div className="place-order-right">
@@ -56,7 +115,7 @@ export const PlaceOrder = () => {
                 </b>
               </div>
           </div>
-          <button>PROCESS TO PAYMENT</button>
+          <button type='submit'>PROCESS TO PAYMENT</button>
         </div>
       </div>
     </form>
