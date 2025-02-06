1e78ef657b5c7c28264b9d97a628b6c74994482a
Retno Ekayanti <retnoekayanti15@gmail.com>
2025-02-06 13:37:04 +0700

    update navbar : navigate to myorders

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import './PlaceOrder.css'
 import { StoreContext } from '../../context/StoreContext';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export const PlaceOrder = () => {
 
@@ -64,6 +65,16 @@ export const PlaceOrder = () => {
       alert("Error")
     }
   }
+
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    if (!token){
+      navigate('/cart')
+    } else if (getTotalCartAmount()===0){
+      navigate('/cart')
+    }
+  },[token])
 
   return (
     <form onSubmit={placeOrder} className='place-order'>
