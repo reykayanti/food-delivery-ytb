d5e1822f7e25c4d2282c3974612cbe6902785295
Retno Ekayanti <retnoekayanti15@gmail.com>
2025-02-06 11:47:10 +0700

    add my order page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import { Home } from './pages/Home/Home'
 import { Cart } from './pages/Cart/Cart'
 import { PlaceOrder } from './pages/PlaceOrder/PlaceOrder'
 import Verify from './pages/Verify/verify'
+import MyOrders from './pages/MyOrders/MyOrders'
 
 const App = () => {
 
@@ -24,6 +25,7 @@ const App = () => {
           <Route path='/cart' element={<Cart/>} />
           <Route path='/order' element={<PlaceOrder/>} />
           <Route path='/verify' element={<Verify/>} />
+          <Route path='/myorders' element={<MyOrders/>}/>
         </Routes>
       </div>
       <Footer />
